1195b2b448e75816dd5c31ec129bf5a8c9e62e98
Vladimir Cherevko <anuris@ukr.net>
2022-04-24 17:17:02 +0300

    task8

--- contacts.py
+++ contacts.py
@@ -342,3 +342,29 @@ class Asisstant:
         result = self.address_book.find_record(name)
         print(result)
 
+"""
+8. Редактировать и удалять заметки
+"""
+def del_note(self, number: int):
+    """Method to delete note by number"""
+    try:
+        number = int(number)
+    except ValueError as e:
+        print(f"Wrong input: {e}, must be an integer")
+    del_note = None
+    for note in self.data:
+        if number == note.number:
+            del_note = self.data.pop(number)# or pop(number-1)?
+            print(f"Note number:{number} was deleted")
+        else:
+            print(f"Numder:{number} is not valide")
+
+def change_note(self, number: str, new_note: str):
+    """Method searches for a note by number and changes its text"""
+    try:
+        number = int( number)
+    except ValueError as e:
+        print(f"Wrong input: {e}, must be an integer")
+    for note in self.data:
+        if number == note.number:
+            note.text = new_note
