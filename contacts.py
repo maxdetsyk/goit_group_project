1d83034b44fd03c1fa19088a3297e8421f751de2
Polina Gotovtseva <gtpolina@gmail.com>
2022-04-28 14:19:43 +0200

    finale1

--- contacts.py
+++ contacts.py
@@ -1,165 +1,98 @@
 from collections import UserDict
-from datetime import date, datetime
-import os
+from datetime import datetime
 import pickle
 import re
+from copy import copy
+
+import os
 
 
-def clear_phone(phone):
-    return (
-        phone.strip()
-        .removeprefix("+38")
-        .replace("(", "")
-        .replace(")", "")
-        .replace("-", "")
-        .replace(" ", "")
-    )
+
+
 
 class Field:
-    def __intit__(self, value) -> None:
-        self.__value = value
+    
+    def __init__(self, value):
+        self._value = None
+        self.value = value
 
+    def __str__(self) -> str:
+        return self._value
+    
     @property
     def value(self):
-        return self.__value
-
+        return self._value
+    
     @value.setter
-    def value(self, new_value):
-        self.value = new_value
+    def value(self,value):
+        self._value = value
 
-
+    
 class Name(Field):
+    
     pass
 
 
+
 class Phone(Field):
-    def __init__(self, value) -> None:
-        super().__init__(value)
-        self.value = value
+    
+    @Field.value.setter
+    def value(self, value):
+        
+        matched_phone = re.findall(r'^\+?\d+$',value)
+        
+        if (matched_phone and len(value) <= 15) or value == '':
+            self._value = value           
+        elif not matched_phone:
+            print("Phone has to be no more then 15 digits and include or not '+' in the beginning")
+            self._value = 'nothing'
+        
+        
 
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, new_value):
-        if Phone.check_phone(new_value):
-            self.__value = clear_phone(new_value)
-        else:
-            print("Phone have wrong format. Try again!")
-
-    def __eq__(self, other: object) -> bool:
-        return self.value == other.value
-
-    @staticmethod
-    def check_phone(phone):
-        new_phone = clear_phone(phone)
-        if len(new_phone) == 10 and new_phone.isdigit():
-            return True
-        else:
-            return False
+            
 
 
-class Birthday:
-    def __init__(self, value):
-        Birthday.__check_date(value)
-        self.__value = datetime.strptime(
-            re.sub("[- //]", ".", value), "%d.%m.%Y")
-
-    def __repr__(self):
-        return f"{self.__value}"
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, new_value):
-        if Birthday.__check_date(new_value):
-            self.__value = new_value
-        else:
-            print("Date have wrong format. Try again!")
-
-    @staticmethod
-    def __check_date(date):
-        date_form = re.sub("[- //]", ".", date)
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, value):
         try:
-            data = datetime.strptime(date_form, "%d.%m.%Y")
-            return str(data)
+            b_day = datetime.strptime(value, "%d/%m/%y").date()
+            self._value = b_day
+
         except ValueError:
-            return None
+            print("Wrong birthday format, has to be dd/mm/yy ")
 
-
-class Email:
-    def __init__(self, value):
-        Email.check_email(value)
-        self.__value = value
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, new_value):
-        if Email.check_email(new_value):
-            self.__value = new_value
-        else:
-            print("Email have wrong format. Try again!")
-
-    @staticmethod
-    def check_email(email):
-        result = re.match(r"[a-zA-Z]\.?\S+@\w{1,6}\.[\w]{1,4}", email)
-        return result
-
-    def __eq__(self, other: object) -> bool:
-        return self.value == other.value
-
-
-class Address:
-    def __init__(self, value):
-        self.__value = value
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, new_value):
-        self.__value = new_value
 
 
 class Record:
-
-    def __init__(self,
-                 name: Name,
-                 birthday: Birthday,
-                 email: Email,
-                 address: Address,
-                 *args) -> None:
-
+    
+    def __init__(self, name, *args):
+        
         self.name = name
         self.phones = []
-        self.birthday = ''
-        self.email = ''
-        self.address = ''
+        self.birthday = ''       
         for item in args:
             if isinstance(item, Phone):
                 self.phones.append(item)
-        if isinstance(birthday, Birthday):
-            self.birthday = birthday
-        if isinstance(email, Email):
-            self.email = email
-        if isinstance(address, Address):
-            self.address = address
-
-    def add_phone(self, phone_number: str) -> list:
-        phone = Phone(phone_number)
-        if phone not in self.phones:
-            self.phones.append(phone)
-
-    # changing phone in record with 2 string arguments in method
-
-    def change_phone(self, phone_arg: str, replace_phone_arg: str):
+            if isinstance(item, Birthday):
+                self.birthday = item
+                              
+            
+                
+                
+    def delete_phone(self, phone_arg):
+        
+        for item in self.phones:
+            if phone_arg == item.value:
+                a = self.phones.remove(item)                
+                return a
+            
+        print(f'No such phone number {phone_arg} in record {self.name}')
+                
+    def add_phone(self, phone_arg):
+        self.phones.append(Phone(phone_arg))
+        
+    def change_phone(self,phone_arg, replace_phone_arg):
         count = 0
         for i in range(len(self.phones)):
             if phone_arg == self.phones[i].value:
@@ -167,115 +100,118 @@ class Record:
                 count += 1
         if count == 0:
             print(f'No such phone {phone_arg} in a record')
-
-    def delete_phone(self, phone):
-
-        for item in self.phone:
-            if phone.value == item.value:
-                new_phones_list = self.phones.remove(item)
-                return new_phones_list
-
-        print(f'No such phone number {phone} in record {self.name}')
-
-    def add_email(self, email):
-        if email not in self.email:
-            self.email = email
-
-    def edit_birthday(self, new_birthday):
-        self.birthday.value = new_birthday
-
-    def edit_email(self, new_email):
-        self.email.value = new_email
-
-    def edit_address(self, new_address):
-        self.address.value = new_address
-
-    def __repr__(self) -> str:
-        return f"Name: {self.name.value}\
-                Phones: {', '.join([str(phone.value) for phone in self.phones])}\
-                Birthday: {self.birthday}\
-                Email:{self.email}\
-                Address: {self.address}"
-
+            
     def days_to_birthday(self):
-        """
-        If birthday is added: counts days before next one.
-        """
-
-        if self.birthday.value is None:
-            print(f"No b-day added for {self.name.value}")
-            return
-
-        date_now = date.today()
-        birthday_date = self.birthday.value
-        birthday_date = birthday_date.replace(year=date_now.year)
-        # Check if user's birthday passed this year => year + 1
-        if birthday_date <= date_now:
-            birthday_date = birthday_date.replace(year=date_now.year + 1)
-
-        days_delta = birthday_date - date_now
-        return days_delta.days
-
-    def birthday_in_next_x_days(self, step: int = 7):
-        try:
-            step = int(step)
-        except ValueError:
-            raise ValueError("Input a number")
-        result = []
-        for record in self.data.values():
-            if record.birthday and record.birthday.value:
-                if record.days_to_birthday() <= step:
-                    birthday = f'{record.name.value} {record.birthday.value.strftime("%d-%m-%Y")}'
-                    result.append(birthday)
-        return result
-
-
-class AddressBookIterator:
-    def __init__(self, data, count_records) -> None:
-        self.data = data
-        self.curr_index = 0
-        self.number_of_records = count_records
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.curr_index < len(self.data):
-            to_show = list(self.data.items())[
-                self.curr_index: min(
-                    len(self.data), self.curr_index + self.number_of_records
-                )
-            ]
-            self.curr_index += self.number_of_records
-            return to_show
+        if not self.birthday:
+            return 'No birthday in this record'
+        
+        
+        today = datetime.today()
+                
+        if (today.month == self.birthday.value.month and today.day >= self.birthday.value.day or today.month > self.birthday.value.month):
+            next_birthday_year = today.year +  1
         else:
-            raise StopIteration
+            next_birthday_year = today.year
+            
+        next_birthday = datetime(next_birthday_year,self.birthday.value.month,self.birthday.value.day)
+        difference = next_birthday - today
+        return  difference.days        
+                 
+        #self.phones = [item if item.value != phone_arg else Phone(replace_phone_arg) for item in self.phones]
+            
+        
+        
+
+    def __str__(self):
+        if isinstance(self.birthday,Birthday):
+            birthday_str = self.birthday.value.strftime("%d/%m/%y")
+            res = f"{self.name}  {', '.join([str(phone) for phone in self.phones])}, {birthday_str}"
+        else:
+            res = f"{self.name}  {', '.join([str(phone) for phone in self.phones])}"
+            
+        return res
+    
 
 
 class AddressBook(UserDict):
-    def __init__(self):
-        UserDict.__init__(self)
 
-    def add_record(self, rec: Record):
-        self.data[rec.name.value] = rec
+    printed_records = 0
+    records_to_print = 0
+    
+    def add_record(self, obj):
 
-    def find_record(self, name: Name) -> Record:
-        return self.data.get(name.value)
-
-    '''def delete_record(self, value: Name):
-        self.data.pop(value.value)'''
-    # AddressBook object method gets as an argument Record object
-
-    def delete_record(self, obj):
+        if isinstance(obj, Record):
+            self.data[obj.name.value] = obj
+            return True
+        
+    def delete_record(self,obj):
         if isinstance(obj, Record):
             del self.data[obj.name.value]
+            
             return self.data
+        
+    def __next__(self):
 
-    def __str__(self) -> str:
-        return str(self.data)
+        if len(self.data.items()) > self.printed_records:
+            
+            printed_chunk = list(self.data.items())[self.printed_records:self.printed_records + self.rows_to_print]
+            printed_chunk_str  = "\n".join([str(value) for key,value in printed_chunk])
+            self.printed_records = self.printed_records + self.rows_to_print
 
-    def iterator(self, count_records) -> AddressBookIterator:
-        return AddressBookIterator(self.data, count_records)
+            return printed_chunk_str
+        else:
+            print("No more records to show")
+            raise StopIteration
+        
+    def __iter__(self):
+         return self
+
+    def iterator_2(self, quantity):
+        self.rows_to_print = quantity
+
+        for record in self:
+            print(record)
+            return record   
+        
+    def iterator(self, rows_number=2):
+        end = len(self.data)
+        i = 0
+        limit = rows_number
+        while True:
+            yield "\n".join([f"{str(item)}" for key,item in list(self.data.items())[i:limit]])
+            print("next page")
+            i, limit = i + rows_number, limit + rows_number
+            if i >= end:
+                break  
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        return attributes
+
+    def __setstate__(self, value):
+        self.__dict__ = value
+
+    def save_to_file(self, file_name):
+        with open(file_name, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self, file_name):
+        with open(file_name, "rb") as file:
+            return pickle.load(file)
+
+    def search(self, text):
+        #result = []
+        for field in self.data.values():
+            if text in field.name.value or [ph for ph in field.phones if text in ph.value]:
+                print(f'{field}----serch result from----{text}')
+        print('-----End of search------')        
+                
+        
+
+    def __str__(self):
+        return "\n".join([f"{str(v)}" for k, v in self.data.items()])
+
+
 
     def save_data(self) -> None:
         folder_sep = "\\"
@@ -295,13 +231,63 @@ class AddressBook(UserDict):
         folder_sep = "\\"
 
         fellow_notes = (
-            os.environ["HOMEPATH"] + folder_sep +
-            "fellow" + folder_sep + "contacts.bin"
+            os.environ["HOMEPATH"] + folder_sep +"fellow" + folder_sep + "contacts.bin"
         )
 
         if os.path.exists(fellow_notes):
             with open(fellow_notes, "rb") as file:
                 self.data = pickle.load(file)
 
+  
 
-CONTACT_BOOK = AddressBook()
+
+def show_contacts():
+    a = str(adress_book)
+    if len(a) == 0:
+        print('The Adress Book is empty')
+    else:
+        print(adress_book)
+    return True
+
+def add_contact():
+    operative_name = input('Enter name of contact:\n')
+    
+    while operative_name == '' or operative_name in adress_book.data.keys():
+        print('Hey, this name exists or it\'s not good enough\n')
+        operative_name = input('Enter name of contact:\n')
+    rec = Record(Name(operative_name))   
+    adress_book.add_record(rec)
+    
+    print('Now add phone number...')
+    operative_phone = input()
+    ph = Phone(operative_phone)       
+    while ph.value == "nothing":
+        print('Add one phone number its obligatory...')
+        operative_phone = input()
+        ph = Phone(operative_phone)
+        if operative_phone == '':
+            print('Okok, fellow, let it be only the the name for now...\n\n')
+            break
+    rec.add_phone(operative_phone)
+    adress_book.save_data()
+    print('Basic contact record created\nTo add or change stuff start from MENU all over again...\nYes i like to torture HAHAHA...;)')
+    return True
+    #adress_book.save_data()
+
+def del_contact(arg):
+    if arg in adress_book.data.keys():
+        del adress_book.data[arg]
+        print('Done!!! Out of the contact book!!!!...\n')
+        adress_book.save_data()
+    else:
+        print('You did not tell me to remember this record before ...\nIknow U know what to do for adding;)\n')
+        return False
+        
+def find_contact(arg):
+    adress_book.search(arg)
+    return True
+
+adress_book = AddressBook()
+adress_book.load_data()
+
+
